f14f5b6d3beae9671f4e5da871ee30104e238192
Mauro Morales <contact@mauromorales.com>
2016-11-24 11:18:42 +0100

    script to install base packages

---
+++ install_packages.sh
@@ -0,0 +1,18 @@
+zypper ar -f http://download.opensuse.org/repositories/utilities/openSUSE_Leap_42.2/ utilities
+zypper in -y \
+	autoconf \
+	automake \
+	bash-completion \
+	cmake \
+  direnv \
+	git \
+	jq \
+  go \
+	gpaste \
+	gnutls \
+	htop \
+	lastpass-cli \
+	the_silver_searcher \
+	tmux \
+	tree \
+	xz \
